b659125fd5cee4eeb1cd0b943a8df0a49b5b00be
imabhishekmahli <abhi8h3k@gmail.com>
2024-06-09 10:31:37 +0530

    page css and JS clickable button

---
+++ script.js
@@ -0,0 +1,13 @@
+rock_btn = document.querySelector("#rock")
+paper_btn = document.querySelector("#paper")
+scissor_btn = document.querySelector("#scissor")
+
+function rock_button(){
+    alert('rock is clicked')
+}
+function paper_button(){
+    alert('rock is clicked')
+}
+function scissor_button(){
+    alert('rock is clicked')
+}
