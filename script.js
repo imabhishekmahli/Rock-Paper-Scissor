1c23bd3cfba2454edd0f6caf226eccede4e02c2d
imabhishekmahli <abhi8h3k@gmail.com>
2024-06-09 11:06:09 +0530

    images added to button and made them functional

--- script.js
+++ script.js
@@ -3,11 +3,38 @@ paper_btn = document.querySelector("#paper")
 scissor_btn = document.querySelector("#scissor")
 
 function rock_button(){
-    alert('rock is clicked')
+    let random_var = Math.random()*3;
+    let computerChoose
+    if(random_var>=0 && random_var<=1){
+        computerChoose = 'rock';
+    }else if(random_var>1 && random_var<=2){
+        computerChoose = 'paper';
+    }else{
+        computerChoose = 'scissor';
+    }
+    alert(`computer have choosen ${computerChoose}`)
 }
 function paper_button(){
-    alert('rock is clicked')
+    let random_var = Math.random()*3;
+    let computerChoose
+    if(random_var>=0 && random_var<=1){
+        computerChoose = 'rock';
+    }else if(random_var>1 && random_var<=2){
+        computerChoose = 'paper';
+    }else{
+        computerChoose = 'scissor';
+    }
+    alert(`computer have choosen ${computerChoose}`)
 }
 function scissor_button(){
-    alert('rock is clicked')
+    let random_var = Math.random()*3;
+    let computerChoose
+    if(random_var>=0 && random_var<=1){
+        computerChoose = 'rock';
+    }else if(random_var>1 && random_var<=2){
+        computerChoose = 'paper';
+    }else{
+        computerChoose = 'scissor';
+    }
+    alert(`computer have choosen ${computerChoose}`)
 }
